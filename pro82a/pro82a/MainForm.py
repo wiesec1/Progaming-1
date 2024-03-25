c207d82cb1280e8ea2c11098213bc04312b2de8b
wiesec1 <162478458+wiesec1@users.noreply.github.com>
2024-03-25 10:29:15 -0500

    3/25/24

--- pro82a/pro82a/MainForm.py
+++ pro82a/pro82a/MainForm.py
@@ -9,9 +9,143 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
+		self._label1 = System.Windows.Forms.Label()
+		self._label2 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._textBox2 = System.Windows.Forms.TextBox()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.PaleGreen
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(13, 13)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(210, 38)
+		self._label1.TabIndex = 0
+		self._label1.Text = "speed limit"
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.PaleGreen
+		self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(13, 67)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(210, 38)
+		self._label2.TabIndex = 1
+		self._label2.Text = "vehicle speed"
+		# 
+		# textBox1
+		# 
+		self._textBox1.BackColor = System.Drawing.Color.PaleGreen
+		self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox1.Location = System.Drawing.Point(251, 13)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(128, 44)
+		self._textBox1.TabIndex = 2
+		# 
+		# textBox2
+		# 
+		self._textBox2.BackColor = System.Drawing.Color.PaleGreen
+		self._textBox2.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox2.Location = System.Drawing.Point(251, 61)
+		self._textBox2.Name = "textBox2"
+		self._textBox2.Size = System.Drawing.Size(128, 44)
+		self._textBox2.TabIndex = 3
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.Color.PaleGreen
+		self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.Location = System.Drawing.Point(13, 131)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(76, 35)
+		self._label3.TabIndex = 4
+		self._label3.Text = "fine"
+		self._label3.Click += self.Label3Click
+		# 
+		# label4
+		# 
+		self._label4.BackColor = System.Drawing.Color.PaleGreen
+		self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label4.Location = System.Drawing.Point(120, 131)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(210, 38)
+		self._label4.TabIndex = 5
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.PaleGreen
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.Color.Black
+		self._button1.Location = System.Drawing.Point(12, 187)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(167, 53)
+		self._button1.TabIndex = 6
+		self._button1.Text = "cauculate"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.PaleGreen
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.ForeColor = System.Drawing.Color.Black
+		self._button2.Location = System.Drawing.Point(185, 187)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(95, 53)
+		self._button2.TabIndex = 7
+		self._button2.Text = "clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.PaleGreen
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 24, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.ForeColor = System.Drawing.Color.Black
+		self._button3.Location = System.Drawing.Point(286, 187)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(95, 53)
+		self._button3.TabIndex = 8
+		self._button3.Text = "exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
 		# 
 		# MainForm
 		# 
+		self.BackColor = System.Drawing.Color.ForestGreen
+		self.ClientSize = System.Drawing.Size(388, 255)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._textBox2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._label1)
 		self.Name = "MainForm"
 		self.Text = "pro82a"
+		self.ResumeLayout(False)
+		self.PerformLayout()
 
+
+	def Label3Click(self, sender, e):
+		pass
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
+
+	def Button2Click(self, sender, e):
+		self._textBox1.Text = " "
+		self._textBox2.Text = " "
+		self._label4.Text = " "
+
+	def Button1Click(self, sender, e):
+		self._textBox2 - textBox1.Tezt
