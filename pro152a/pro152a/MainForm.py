a9316bd40af6062668028dbb1545600364bf59aa
wiesec1 <162478458+wiesec1@users.noreply.github.com>
2024-04-29 09:54:44 -0500

    4/29/2024

--- pro152a/pro152a/MainForm.py
+++ pro152a/pro152a/MainForm.py
@@ -9,11 +9,23 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
 		self.SuspendLayout()
+		# 
+		# listBox1
+		# 
+		self._listBox1.BackColor = System.Drawing.Color.Olive
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.Location = System.Drawing.Point(12, 12)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(173, 95)
+		self._listBox1.TabIndex = 0
 		# 
 		# MainForm
 		# 
+		self.BackColor = System.Drawing.Color.DarkKhaki
 		self.ClientSize = System.Drawing.Size(834, 408)
+		self.Controls.Add(self._listBox1)
 		self.Name = "MainForm"
 		self.Text = "pro152a"
 		self.ResumeLayout(False)
