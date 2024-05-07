f71395d8ef7fbca58bdbf27f063cee6b5f489634
wiesec1 <162478458+wiesec1@users.noreply.github.com>
2024-05-07 10:30:27 -0500

    5/7/2024

--- pro152a/pro152a/MainForm.py
+++ pro152a/pro152a/MainForm.py
@@ -84,7 +84,9 @@ class MainForm(Form):
 	def Button1Click(self, sender, e):
 		heading = "math\t\t sum"
 		self._listBox1.Items.Add(heading)
+		Sum = 15586428
+		answer = "\t\t" + str(Sum)
+		self._listBox1.Items.Add(answer)
 		for num in range(3, 9670, 3):
-			sum = 3 + 6 + 9 + 12 + 15 + 18 + 21 + 24 + 27 + 30 + 33 + 36 + 39 + 42 + 45 + 48 + 51 + 54 + 57 + 60 + 63 + 66 + 69 + 72 + 75 + 78 + 81 + 84 + 87 + 90 + 93 + 96 + 99 + 102 + 105 + 108 + 111 + 114 + 117 + 120 + 123 + 126 + 129 + 132 + 135+138+141+144+147+150+153+
 			line = str(num)
-			self._listBox1.Items.Add(line)
+			self._listBox1.Items.Add(line) 
